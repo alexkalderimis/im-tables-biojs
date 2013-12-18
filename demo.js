03e0c188bcdfbf17c5ae99aef7986207c96e0722
Alex Kalderimis <alex.kalderimis@gmail.com>
2013-12-18 15:05:09 +0000

    Added im-tables biojs wrapper.

---
+++ demo.js
@@ -0,0 +1,20 @@
+(function main (B) {
+  var table = new B.InterMineTable({
+    target: "table-container",
+    url: "http://www.flymine.org/query",
+    query: {
+      from: 'Organism',
+      select: ['commonName', 'name', 'taxonId']
+    }
+  });
+  window.demoTable = table;
+  table.onQueryChanged(function (query) {
+    console.log("Query is now: " + query.toXML());
+  });
+  table.addListener('imo:click', function (type, id) {
+    console.log("User clicked on " + type + " " + id);
+  });
+  table.onAllEvents(function (eventName, arg0) {
+    console.log("Received event " + eventName + ". First arg: " + arg0);
+  });
+})(window.Biojs);
